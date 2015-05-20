111f9cbaf96f96548df6c6a610dea3b0e5b364ab
Jagaa <jagaa1103@gmail.com>
2015-05-20 21:01:50 +0900

    all views edited

--- js/contactCtrl.js
+++ js/contactCtrl.js
@@ -1,3 +1,19 @@
 app.controller('contactCtrl', function($scope){
 	console.log("contactCtrl");
+	initialize();
+	function initialize() {
+	  var mapProp = {
+	    center: new google.maps.LatLng(37.5645557,126.9849151),
+	    zoom:12,
+	    panControl: false,
+	    zoomControl: true,
+	    scaleControl: false,
+	    mapTypeControl: false,
+	    streetViewControl: false,
+	    scrollwheel: false,
+	    mapTypeId:google.maps.MapTypeId.ROADMAP
+	  };
+	  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
 });
